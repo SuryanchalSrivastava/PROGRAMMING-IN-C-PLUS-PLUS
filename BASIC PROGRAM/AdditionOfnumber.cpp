90274cdf33485059fe17ad7dac47bc46c8d4f241
Suryanchal Srivastava <suryanchalkumar.vashu@gmail.com>
2024-04-18 00:31:46 +0530

    first commit

---
+++ BASIC PROGRAM/AdditionOfnumber.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+    int main()
+    {
+        int num1,num2,sum;
+        cout<<"Enter the num1:";
+        cin>>num1;
+        cout<<"Enter the num2:";
+        cin>>num2;
+        sum=num1+num2;
+        cout<<"Addition is:"<<sum;
+        return 0;
+    }
